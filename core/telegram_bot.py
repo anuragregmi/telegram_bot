42e00df2a7672ae0f4b53798097fdd080f9c8d16
Anurag Regmi <anuragregmi@protonmail.com>
2018-04-14 12:19:28 +0545

    Seperate handlers for group and private message

--- core/telegram_bot.py
+++ core/telegram_bot.py
@@ -10,17 +10,35 @@ class TelegramBot:
 
     def handle_message(self, msg):
         message = Message(msg)
-
         if message.content_type == 'text':
+            if message.chat_type == 'group':
+                self.handle_group_message(message)
+            elif message.chat_type == 'private':
+                self.handle_private_message(message)
+
+    def handle_private_message(self, message):
+        if message.sender.username in config.KNOWN_USERS:
+            self.bot.sendMessage(
+                message.chat_id,
+                "I am working on private chat at the moment"
+            )
+
+    def handle_group_message(self, message):
+        username = '@{}'.format(config.USERNAME)
+
+        if username in message.text:
+            print(message.sender.username, message.sender.id, message.text)
             if message.sender.username in config.KNOWN_USERS:
-                self.bot.sendMessage(
-                    message.chat_id,
-                    "{} bhai maile chinxu ni timilai.".format(
-                    message.sender.first_name)
-                )
+                reply_message= ""
+                text = message.text.lower()
+                for reply in config.REPLIES:
+                    if reply in text:
+                        reply_message += "{} ".format(config.REPLIES[reply])
+
+                if reply_message.__len__() != 0:
+                    self.bot.sendMessage(message.chat_id, reply_message)
             else:
-                self.bot.sendMessage(
-                    message.chat_id, "Maile chinena ni tapailai.")
+                self.bot.sendMessage(message.chat_id, config.UNKNOWN_USER_REPLY)
 
     def run_bot(self):
         MessageLoop(
